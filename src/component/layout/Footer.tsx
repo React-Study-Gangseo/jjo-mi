6a3f2f45220baa827c82c29d22b55eb9a1951448
jjo-mi <whdal2636@naver.com>
2024-01-10 22:52:29 +0900

    [style] 푸터 반응형으로 변경

--- src/component/layout/Footer.tsx
+++ src/component/layout/Footer.tsx
@@ -7,84 +7,91 @@ import snsYt from "../../assets/images/icon-yt.svg";
 
 const Footer: React.FC = () => {
   return (
-    <FooterDiv>
-      <ContentWrapper>
-        <LinkContainer>
-          <FooterLinks aria-label="주요 페이지 링크">
-            <li>
-              <Link to="/" aria-label="회사 소개 페이지로 이동">
-                호두샵 소개
-              </Link>
-            </li>
-            <li>
-              <Link to="/" aria-label="이용약관 페이지로 이동">
-                이용약관
-              </Link>
-            </li>
-            <li>
-              <Link to="/" aria-label="개인정보처리방침 페이지로 이동">
-                개인정보처리방침
-              </Link>
-            </li>
-            <li>
-              <Link to="/" aria-label="전자금융거래약관 페이지로 이동">
-                전자금융거래약관
-              </Link>
-            </li>
-            <li>
-              <Link to="/" aria-label="청소년보호정책 페이지로 이동">
-                청소년보호정책
-              </Link>
-            </li>
-            <li>
-              <Link to="/" aria-label="제휴문의 페이지로 이동">
-                제휴문의
-              </Link>
-            </li>
-          </FooterLinks>
-          <SnsImgContainer>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="인스타그램"
-            >
-              <img src={snsInsta} alt="sns-instagram" />
-            </a>
-            <a
-              href="https://ko-kr.facebook.com/"
-              target="_blank"
-              rel="페이스북"
-            >
-              <img src={snsFb} alt="sns-facebook" />
-            </a>
-            <a href="https://www.youtube.com/" target="_blank" rel="유튜브">
-              <img src={snsYt} alt="sns-youtube" />
-            </a>
-          </SnsImgContainer>
-        </LinkContainer>
-        <Line />
-        <CompanyInfo>
-          <strong>(주)HODU SHOP</strong>
-          <br />
-          서울시 구로구 신도림
-          <br />
-          사업자 번호 : 000-0000-00000 | 통신판매업
-          <br />
-          대표: 정종미
-        </CompanyInfo>
-      </ContentWrapper>
-    </FooterDiv>
+    <Container>
+      <FooterDiv>
+        <ContentWrapper>
+          <LinkContainer>
+            <FooterLinks aria-label="주요 페이지 링크">
+              <li>
+                <Link to="/" aria-label="회사 소개 페이지로 이동">
+                  호두샵 소개
+                </Link>
+              </li>
+              <li>
+                <Link to="/" aria-label="이용약관 페이지로 이동">
+                  이용약관
+                </Link>
+              </li>
+              <li>
+                <Link to="/" aria-label="개인정보처리방침 페이지로 이동">
+                  개인정보처리방침
+                </Link>
+              </li>
+              <li>
+                <Link to="/" aria-label="전자금융거래약관 페이지로 이동">
+                  전자금융거래약관
+                </Link>
+              </li>
+              <li>
+                <Link to="/" aria-label="청소년보호정책 페이지로 이동">
+                  청소년보호정책
+                </Link>
+              </li>
+              <li>
+                <Link to="/" aria-label="제휴문의 페이지로 이동">
+                  제휴문의
+                </Link>
+              </li>
+            </FooterLinks>
+            <SnsImgContainer>
+              <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="인스타그램"
+              >
+                <img src={snsInsta} alt="sns-instagram" />
+              </a>
+              <a
+                href="https://ko-kr.facebook.com/"
+                target="_blank"
+                rel="페이스북"
+              >
+                <img src={snsFb} alt="sns-facebook" />
+              </a>
+              <a href="https://www.youtube.com/" target="_blank" rel="유튜브">
+                <img src={snsYt} alt="sns-youtube" />
+              </a>
+            </SnsImgContainer>
+          </LinkContainer>
+          <Line />
+          <CompanyInfo>
+            <strong>(주)HODU SHOP</strong>
+            <br />
+            서울시 강남구
+            <br />
+            사업자 번호 : 000-0000-00000 | 통신판매업
+            <br />
+            대표: 정종미
+          </CompanyInfo>
+        </ContentWrapper>
+      </FooterDiv>
+    </Container>
   );
 };
+const Container = styled.footer`
+  width: 100vw;
+  background-color: var(--greyF2);
+`;
 
 const FooterDiv = styled.div`
-  width: 100vw;
-  background-color: #f2f2f2;
+  max-width: 80rem;
+  margin: 0 auto;
+  padding: 1.25rem;
 `;
 const ContentWrapper = styled.section`
-  max-width: 1250px;
+  max-width: 78.125rem;
   margin: 0 auto;
-  padding-top: 30px;
+  padding-top: 1.875rem;
 `;
 
 const LinkContainer = styled.div`
@@ -95,7 +102,7 @@ const LinkContainer = styled.div`
 
 const SnsImgContainer = styled.article`
   display: flex;
-  gap: 14px;
+  gap: 0.875rem;
 `;
 
 const FooterLinks = styled.ul`
@@ -103,7 +110,7 @@ const FooterLinks = styled.ul`
 
   & > li {
     display: inline-block;
-    margin-right: 14px;
+    margin-right: 0.875rem;
 
     &:last-child {
       margin-right: 0;
@@ -111,12 +118,12 @@ const FooterLinks = styled.ul`
 
     &:not(:last-child)::after {
       content: "|";
-      margin-left: 14px;
+      margin-left: 0.875rem;
     }
 
     & > a {
       text-decoration: none;
-      color: #333333;
+      color: var(--main-text-color);
       &:hover {
         text-decoration: none;
         font-weight: bold;
@@ -127,13 +134,13 @@ const FooterLinks = styled.ul`
 
 const Line = styled.hr`
   width: 100%;
-  margin: 30px auto;
+  margin: 1.875rem auto;
 `;
 
 const CompanyInfo = styled.section`
-  color: #767676;
-  font-size: 14px;
-  line-height: 24px;
+  color: var(--grey76);
+  font-size: 0.875rem;
+  line-height: 1.5rem;
 `;
 
 export default Footer;
