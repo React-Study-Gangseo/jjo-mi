a31c37532fc595117eedd971eedb8322e2fb9940
jjo-mi <whdal2636@naver.com>
2024-01-10 18:02:40 +0900

    [style] 상품상세페이지 상세정보 및 리뷰 탭 링크에서 하단에 바로 보여지는 형태로 변경

--- src/page/productpage/DetailMenus.tsx
+++ src/page/productpage/DetailMenus.tsx
@@ -1,81 +1,83 @@
 import React, { useState } from "react";
-import { NavLink, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
+// 스타일
 const MenuStyle = styled.ul`
   width: 100%;
   display: flex;
   margin: 140px 0;
 `;
-const ListStyle = styled.li`
-  border-bottom: 5px solid #e0e0e0;
+const TabStyle = styled.li<{ isActive: boolean }>`
+  border-bottom: 5px solid
+    ${(props) => (props.isActive ? "var(--main-color)" : "var(--grayE0)")};
   text-align: center;
   flex: 1 1 auto;
+  cursor: pointer;
 `;
 
-const activeLinkStyle = {
+const linkStyle = {
   display: "block",
-  padding: "20px 0",
-  textDecoration: "none",
+  padding: "1.25rem 0",
+  fontSize: "1.125rem",
+};
+
+const activeLinkStyle = {
+  ...linkStyle,
   color: "var(--main-color)",
-  fontSize: "18px",
 };
 
 const inactiveLinkStyle = {
-  display: "block",
-  padding: "18px 0",
-  // textDecoration: "none",
-  fontSize: "18px",
+  ...linkStyle,
   color: "var(--grey76)",
 };
 
+const ContentContainer = styled.div`
+  margin-top: 1.25rem;
+
+  text-align: center;
+  height: 30rem;
+`;
+
+const tabs = [
+  { id: "description", label: "상세정보" },
+  { id: "recipe", label: "리뷰" },
+  { id: "review", label: "Q&A" },
+  { id: "returnExchange", label: "반품/교환정보" },
+];
+
 export const DetailMenus: React.FC = () => {
-  const { id } = useParams();
+  const [selectedTab, setSelectedTab] = useState(tabs[0].id);
+
+  const handleTabClick = (tabId: string) => {
+    setSelectedTab(tabId);
+  };
 
   return (
     <div>
       <MenuStyle>
-        <ListStyle>
-          <NavLink
-            to={`/kimchis/${id}/description`}
-            style={({ isActive }) =>
-              isActive ? activeLinkStyle : inactiveLinkStyle
-            }
+        {tabs.map((tab) => (
+          <TabStyle
+            key={tab.id}
+            onClick={() => handleTabClick(tab.id)}
+            isActive={selectedTab === tab.id}
           >
-            버튼
-          </NavLink>
-        </ListStyle>
-        <ListStyle>
-          <NavLink
-            to={`/kimchis/${id}/recipe`}
-            style={({ isActive }) =>
-              isActive ? activeLinkStyle : inactiveLinkStyle
-            }
-          >
-            리뷰
-          </NavLink>
-        </ListStyle>
-        <ListStyle>
-          <NavLink
-            to={`/kimchis/${id}/review`}
-            style={({ isActive }) =>
-              isActive ? activeLinkStyle : inactiveLinkStyle
-            }
-          >
-            Q&A
-          </NavLink>
-        </ListStyle>
-        <ListStyle>
-          <NavLink
-            to={`/kimchis/${id}/review`}
-            style={({ isActive }) =>
-              isActive ? activeLinkStyle : inactiveLinkStyle
-            }
-          >
-            반품/교환정보
-          </NavLink>
-        </ListStyle>
+            <div
+              style={
+                selectedTab === tab.id ? activeLinkStyle : inactiveLinkStyle
+              }
+            >
+              {tab.label}
+            </div>
+          </TabStyle>
+        ))}
       </MenuStyle>
+      <ContentContainer>
+        {selectedTab === "description" && <div>상세정보 페이지</div>}
+        {selectedTab === "recipe" && <div>리뷰 페이지</div>}
+        {selectedTab === "review" && <div>Q&A 페이지</div>}
+        {selectedTab === "returnExchange" && <div>반품/교환정보 페이지</div>}
+      </ContentContainer>
     </div>
   );
 };
